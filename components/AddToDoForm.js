b7a422bd16e12e57945f95daf7bc96784bcd9ef4
DeadBoyPiotrek <87661735+DeadBoyPiotrek@users.noreply.github.com>
2022-01-26 19:43:31 +0100

    add form

--- components/AddToDoForm.js
+++ components/AddToDoForm.js
@@ -1,17 +1,54 @@
 import React from 'react';
-import styles from './ToDo.module.scss';
-function ToDo() {
+import styles from './AddToDoForm.module.scss';
+import { useForm } from 'react-hook-form';
+function AddToDoForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = data => {
+    console.log(data);
+  };
+
   return (
     <>
-      <div className={styles.container}>
-        <div className={styles.container_title}>buy bread</div>
-        <div className={styles.container_box}></div>
-        <div className={styles.container_description}>
-          make sure to get fresh one
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={styles.container}>
+          <div className={styles.container__title}>
+            <input
+              placeholder="title"
+              {...register('title', { required: true })}
+            ></input>
+          </div>
+          <div className={styles.container__importance}>
+            <select {...register('importance')}>
+              <option value="A">A</option>
+              <option value="B">B</option>
+              <option value="C">C</option>
+              <option value="D">D</option>
+            </select>
+          </div>
+          <div className={styles.container__importanceValue}>
+            <select {...register('importanceValue')}>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+          <div className={styles.container__description}>
+            <input
+              placeholder="description"
+              {...register('description', { required: true })}
+            ></input>
+          </div>
+          <button>Add to do</button>
         </div>
-      </div>
+      </form>
     </>
   );
 }
 
-export default ToDo;
+export default AddToDoForm;
--- components/ToDo.js
+++ components/ToDo.js
@@ -3,9 +3,9 @@ function ToDo() {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.container_title}>buy bread</div>
-        <div className={styles.container_box}></div>
-        <div className={styles.container_description}>
+        <div className={styles.container__title}>buy bread</div>
+        <div className={styles.container__box}></div>
+        <div className={styles.container__description}>
           make sure to get fresh one
         </div>
       </div>
