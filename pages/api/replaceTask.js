c3227432a6a5fb23857f9c7c26c7a51724c64ef5
DeadBoyPiotrek <87661735+DeadBoyPiotrek@users.noreply.github.com>
2023-02-17 13:53:24 +0100

    Update replaceTask.js

--- pages/api/replaceTask.js
+++ pages/api/replaceTask.js
@@ -33,10 +33,10 @@ const handler = async (req, res) => {
   }
 
   try {
-    const client = await clientPromise;
-    const db = await client.db();
+//     const client = await clientPromise;
+//     const db = await client.db();
 
-    db.collection('tasks').replaceOne({ _id: new mongodb.ObjectID(id) }, data);
+//     db.collection('tasks').replaceOne({ _id: new mongodb.ObjectID(id) }, data);
     res.status(200).json({ data, name: 'successfully connected to database' });
   } catch (err) {
     res.status(500).json({ message: `err: ${err}` });
