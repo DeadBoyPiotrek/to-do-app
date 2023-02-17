1ddede07280e8218be2e28067846f5829558ec8f
DeadBoyPiotrek <87661735+DeadBoyPiotrek@users.noreply.github.com>
2023-02-17 13:52:56 +0100

    Update deleteTask.js

--- pages/api/deleteTask.js
+++ pages/api/deleteTask.js
@@ -4,10 +4,10 @@ const handler = async (req, res) => {
   const id = req.body.id;
   // console.log('id in api:', id);
   try {
-    const client = await clientPromise;
-    const db = await client.db();
+//     const client = await clientPromise;
+//     const db = await client.db();
 
-    db.collection('tasks').deleteOne({ _id: new mongodb.ObjectID(id) });
+//     db.collection('tasks').deleteOne({ _id: new mongodb.ObjectID(id) });
 
     res.status(200).json({ name: 'successfully connected to database' });
   } catch (err) {
